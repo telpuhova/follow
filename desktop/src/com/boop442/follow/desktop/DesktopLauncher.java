a4629bb42acaac35c9acfa406e9e8f28d30684fe
telpuhova <telpuhova@gmail.com>
2018-04-09 10:41:05 -0700

    configure for desktop and html

--- desktop/src/com/boop442/follow/desktop/DesktopLauncher.java
+++ desktop/src/com/boop442/follow/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.boop442.follow.Follow;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Follow";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Follow(), config);
 	}
 }
